401010982c0ed78fceb3408bbd2b55729d561eff
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-06-17 18:01:04 +0000

    SkyChop: restructuring

--- skychop/clipfits.py
+++ skychop/clipfits.py
@@ -4,7 +4,7 @@
 
 import sys
 import os
-os.environ['HOME'] = '/var/www/html/sdss3/apw235/sdss-tmp/'
+os.environ['HOME'] = '/var/www/html/sdss3/skychop/sdss-tmp/'
 from astLib import astCoords
 from astLib import astImages
 from astLib import astWCS
