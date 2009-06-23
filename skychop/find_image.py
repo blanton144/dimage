54825db63e53590e730e3ebce499bd03835ffcc0
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-06-23 16:57:09 +0000

    SkyChop: small php update

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -19,33 +19,29 @@ size = float(sys.argv[3])
 bands = sys.argv[4]
 tarName = sys.argv[5]
 
-try:
-	fileName, fileDir = ic.findClosestCenter(RADeg, decDeg)
-	outDir = '/var/www/html/sdss3/skychop/sdss-tmp/'
+fileName, fileDir = ic.findClosestCenter(RADeg, decDeg)
+outDir = '/var/www/html/sdss3/skychop/sdss-tmp/'
 
-	if fileDir[len(fileDir) - 1] != "/":
-		fileDir = fileDir + "/"
+if fileDir[len(fileDir) - 1] != "/":
+	fileDir = fileDir + "/"
 
-	arcFileList = []
-	for letter in bands:
-		ic.gunzipIt(fileName + "-" + letter + ".fits.gz", fileDir+fileName+"/", outDir)
-		ic.clipFits(outDir + fileName + "-" + letter + ".fits", RADeg, decDeg, size, outDir + fileName + "-" + letter + "-" + str(size) + ".fits")
-		os.unlink(outDir + fileName + "-" + letter + ".fits")
-		arcFileList.append("sdss-tmp/" + fileName + "-" + letter + "-" + str(size) + ".fits")
+arcFileList = []
+for letter in bands:
+	ic.gunzipIt(fileName + "-" + letter + ".fits.gz", fileDir+fileName+"/", outDir)
+	ic.clipFits(outDir + fileName + "-" + letter + ".fits", RADeg, decDeg, size, outDir + fileName + "-" + letter + "-" + str(size) + ".fits")
+	os.unlink(outDir + fileName + "-" + letter + ".fits")
+	arcFileList.append("sdss-tmp/" + fileName + "-" + letter + "-" + str(size) + ".fits")
 
-	tar = tarfile.open(outDir + tarName+".tar", "w")
-	for name in arcFileList:
-		tar.add(name)
-		os.unlink(name)
-	tar.close()
+tar = tarfile.open(outDir + tarName+".tar", "w")
+for name in arcFileList:
+	tar.add(name)
+	os.unlink(name)
+tar.close()
 
-	ic.gzipIt(tarName+".tar", outDir)
-	os.chmod(outDir+tarName+".tar.gz",0777)
+ic.gzipIt(tarName+".tar", outDir)
+os.chmod(outDir+tarName+".tar.gz",0777)
 
-	if os.path.isfile(outDir+tarName+".tar.gz"):
-		print 1
-	else: 
-		print 0
-
-except OSError:
-	print "0"
+if os.path.isfile(outDir+tarName+".tar.gz"):
+	print 1
+else: 
+	print 0
