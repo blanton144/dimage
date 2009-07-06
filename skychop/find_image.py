b11f717290f0857146ed42d071d7f088b12073ac
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-06 18:31:47 +0000

    SkyChop: faster method to find image
    Former-commit-id: 6676dadd0fb3b1dca3d24d88a2dffc18f5bbd958

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -27,7 +27,7 @@ if fileDir[len(fileDir) - 1] != "/":
 
 arcFileList = []
 for letter in bands:
-	ic.gunzipIt(fileName + "-" + letter + ".fits.gz", fileDir+fileName+"/", outDir)
+	ic.gunzipIt(fileName + "-" + letter + ".fits.gz", fileDir+fileName, outDir)
 	ic.clipFits(outDir + fileName + "-" + letter + ".fits", RADeg, decDeg, size, outDir + fileName + "-" + letter + "-" + str(size) + ".fits")
 	os.unlink(outDir + fileName + "-" + letter + ".fits")
 	arcFileList.append("sdss-tmp/" + fileName + "-" + letter + "-" + str(size) + ".fits")
