afdab8d35518799af88406e0a2b338cd44432c92
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-06 16:00:21 +0000

    SkyChop: troubleshooting
    Former-commit-id: b9a3825b6352ca1633dd400a346c4d9891129ff8

--- skychop/convert.php
+++ skychop/convert.php
@@ -56,7 +56,7 @@ if (isset($_POST['submit2'])) {
 </font>
 <?php
 if (isset($_POST['submit3'])) {
-	print "<font class='errorText'>$deg&deg;" + ":" + "$min" + ":" + "$sec</font>";
+	print "<font class='errorText'>$deg&deg;:$min:$sec</font>";
 }
 ?>
 </center>
