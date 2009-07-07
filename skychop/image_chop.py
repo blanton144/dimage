92777317e6407e6d74640a4eab38131845af926d
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-07 17:14:15 +0000

    skychop: faster process
    Former-commit-id: 760190e712148728f2a7afc59145a64178cc38b3

--- skychop/image_chop.py
+++ skychop/image_chop.py
@@ -7,8 +7,8 @@
 
 import os
 # Enable this line for testing
-os.environ['HOME'] = '/var/www/html/sdss3/skychop'
-#os.environ['HOME'] = '/var/www/html/sdss3/skychop/sdss-tmp'
+#os.environ['HOME'] = '/var/www/html/sdss3/skychop'
+os.environ['HOME'] = '/var/www/html/sdss3/skychop/sdss-tmp'
 import numpy as np
 import pyfits as pf
 from math import fabs
