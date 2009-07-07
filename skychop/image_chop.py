c1559bbf53e7a52d1ae8c944c3b96735155a58de
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-07 14:06:49 +0000

    SkyChop: dump finddec()
    Former-commit-id: f53529a1937682defe1a2e495be5e32294434412

--- skychop/image_chop.py
+++ skychop/image_chop.py
@@ -53,7 +53,6 @@ def findClosestCenter(RADeg, decDeg):
 	
 	if degDec > 0.0: RADecPath = "%(path)s%(RAIntHour)02dh/p%(degg)02d/" % {"path":fitsPath,"RAIntHour":RAIntHour,"degg":fabs(degDec)}
 	else: RADecPath = "%(path)s%(RAIntHour)02dh/m%(degg)02d/" % {"path":fitsPath,"RAIntHour":RAIntHour,"degg":fabs(degDec)}
-	print fabs(degDec), degDec, decDecl
 	
 	fileName = "J%(hr)02d%(min)02d%(secINT)02d.%(secDECIMAL)02d%(degDec)+03d%(minDec)02d%(secDecINT)02d.%(secDecDECIMAL)01d" % \
 		{"hr":hr,"min":min,"secINT":secINT,"secDECIMAL":secDECIMAL,"degDec":fabs(degDec),"minDec":minDec,"secDecINT":secDecINT,"secDecDECIMAL":secDecDECIMAL}
