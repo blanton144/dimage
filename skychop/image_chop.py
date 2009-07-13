d76b09104820eb81663147c26d6201c8c2f39404
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-13 14:12:42 +0000

    skychop: typo correction
    Former-commit-id: ccbd0e3a7748cb29cd645874387ac391b0da7bbc

--- skychop/image_chop.py
+++ skychop/image_chop.py
@@ -59,13 +59,13 @@ def getFileName(theRA, theDec, fitsPath):
 		os._exit(0)
 	
 	if degDec > 0.0:
-		if fabs(degDec) % 2 == 0: degDec = fabs(degDec)
-		else: degDec = fabs(degDec) - 1.0
+		if fabs(int(degDec)) % 2 == 0: degDec = fabs(int(degDec))
+		else: degDec = fabs(int(degDec)) - 1
 		RADecPath = "%(path)s%(hrRa)02dh/p%(dec)02d/" % {"path":fitsPath,"hrRa":hrRa,"dec":degDec}
 		print degDec
 	else:
-		if fabs(degDec) % 2 == 0: degDec = fabs(degDec)
-		else: degDec = fabs(degDec) + 1.0
+		if fabs(int(degDec)) % 2 == 0: degDec = fabs(int(degDec))
+		else: degDec = fabs(int(degDec)) + 1
 		RADecPath = "%(path)s%(hrRa)02dh/m%(dec)02d/" % {"path":fitsPath,"hrRa":hrRa,"dec":degDec}
 	
 	fileName = "J%(hrRa)02d%(minRa)02d%(secRaINT)02d.%(secRaDECIMAL)02d%(degDec)+03d%(minDec)02d%(secDecINT)02d.%(secDecDECIMAL)01d" % \
