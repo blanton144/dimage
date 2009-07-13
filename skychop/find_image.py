8cca82ef3fa5f4e07225f4cb384b2943771e712a
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-13 17:00:47 +0000

    skychop: typo correction
    Former-commit-id: adc4bc84a691fc91001ce2fb8acefdeb1d18fdf0

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -62,7 +62,7 @@ for k in range(np.shape(allFileNamesT)[1]):
 	for name in allFileNamesT[k]:
 		swarpArg += " %s" % name
 	coaddFname = ic.getIAUFname(RADeg,decDeg,letter) + "-" + bands[k] + "-" + str(xSize) +"x"+ str(ySize) + ".fits"
-	os.system("swarp%s %s" % (swarpArg,"-IMAGEOUT_NAME="+coaddFname)
+	os.system("swarp%s %s" % (swarpArg,"-IMAGEOUT_NAME="+coaddFname))
 	arcFileList.append("sdss-tmp/" + coaddFname)
 print arcFileList
 os._exit(0)
