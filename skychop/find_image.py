3e4ca411270b67d3432b4d077827d4121eaa61c7
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-14 19:58:58 +0000

    skychop: troubleshooting
    Former-commit-id: f7d4c531b9dc51712e373f10cf76b04443a9a966

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -76,16 +76,16 @@ else:
 		for name in allFileNamesT[k]:
 			swarpArg += " %s" % name
 		coaddFname = ic.getIAUFname(RADeg,decDeg) + "-" + bands[k] + "-" + str(xSize) +"x"+ str(ySize) + ".fits"
-		swarpKARGS = "-IMAGEOUT_NAME=sdss-tmp/" + coaddFname + " -VERBOSE_TYPE=FULL -RESAMPLE_DIR=sdss-tmp -WEIGHTOUT_NAME=sdss-tmp/weight.fits"
+		swarpKARGS = "-IMAGEOUT_NAME=sdss-tmp/" + coaddFname + " -VERBOSE_TYPE=FULL -RESAMPLE_DIR=~ -WEIGHTOUT_NAME=~/weight.fits"
 		os.system("swarp%s %s" % (swarpArg,swarpKARGS))
-		arcFileList.append(outDir + coaddFname)
+		arcFileList.append(coaddFname)
 		for name in allFileNamesT[k]:
 			os.unlink(name)
 
 tar = tarfile.open(outDir + tarName+".tar", "w")
 for fname in arcFileList:
-	tar.add(fname)
-	os.unlink(fname)
+	tar.add("~/"+fname)
+	os.unlink("~/"+fname)
 tar.close()
 
 ic.gzipIt(tarName+".tar", outDir)
