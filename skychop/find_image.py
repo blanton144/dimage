a3ce4e824f3790a6114a6449a3571b561221c2b1
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-14 17:42:33 +0000

    skychop: troubleshooting
    Former-commit-id: da0d6164eb3c4772696b6fb09d5b064be4d236bb

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -76,7 +76,8 @@ else:
 		for name in allFileNamesT[k]:
 			swarpArg += " %s" % name
 		coaddFname = ic.getIAUFname(RADeg,decDeg) + "-" + bands[k] + "-" + str(xSize) +"x"+ str(ySize) + ".fits"
-		os.system("swarp%s %s" % (swarpArg,"-VERBOSE_TYPE=QUIET -IMAGEOUT_NAME=sdss-tmp/"+coaddFname))
+		#os.system("swarp%s %s" % (swarpArg,"-VERBOSE_TYPE=QUIET -IMAGEOUT_NAME=sdss-tmp/"+coaddFname))
+		os.system("swarp%s %s" % (swarpArg," -IMAGEOUT_NAME=sdss-tmp/"+coaddFname))
 		arcFileList.append("sdss-tmp/" + coaddFname)
 
 tar = tarfile.open(outDir + tarName+".tar", "w")
