fc713c5664bcd17a0ae86d9af5d1ad503b7eae6a
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-15 18:32:58 +0000

    skychop: updated web stuff
    Former-commit-id: 96a3eda0bb300822e15b12a2ee12d2d520fcd27e

--- skychop/process.php
+++ skychop/process.php
@@ -1,4 +1,5 @@
 <?php
+	/*
 	// Get variables from form GET
 	if (isset($_SESSION)) {
 		continue;
@@ -13,14 +14,13 @@
 		$_SESSION['fname'] = stripslashes($_GET['fname']);
 		$_SESSION['skychop'] = "/var/www/html/sdss3/skychop";
 	}
-	ini_Set('display_errors',1); // turn on error reporting while developing
 	
 	function wait_get_line($file) {
 		sleep(5);
 		$openfile = fopen("sdss-tmp/$file","r");
 		$line = fread($openfile, 1024);
 		return ($line);
-	}
+	}*/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -31,6 +31,7 @@
 <body>
 <?php
 	print "Hello, world!";
+	
 	if (isset($script_start)) {
 		continue;
 	}
