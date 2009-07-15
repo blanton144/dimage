ccb4767669be4ca3f75a28a4a7c138ea2bbde227
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-15 18:26:21 +0000

    skychop: updated web stuff
    Former-commit-id: fc4202cd35737e1b5c91ff78b1efef1bd4af3bba

--- skychop/process.php
+++ skychop/process.php
@@ -10,6 +10,7 @@
 		$_SESSION['fname'] = stripslashes($_GET['fname']);
 		$_SESSION['skychop'] = "/var/www/html/sdss3/skychop";
 	}
+	ini_Set('display_errors',1); // turn on error reporting while developing
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
