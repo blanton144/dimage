7dce9f6745dc0ac42c48c1dc11dfd7c8eefbc846
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-17 14:20:57 +0000

    skychop: test array compatibility
    Former-commit-id: b82417cb62b1c714cb4f3b8804ff1cf142c14ccb

--- skychop/test.py
+++ skychop/test.py
@@ -6,7 +6,43 @@
 #  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 #
 
-a = [1,6,2,8]
-b = ["a","b","c"]
+def convertArray(arr):
+	ret = ""
+	list = []
+	
+	if isinstance(arr, type([]) ):		## If the instance is an array
+		
+		for ele in arr:
+			if isinstance(ele, ( type([]), type({})) ):	## If the instance is an array
+				list.append( convertArray(ele))					## recursive it
+			
+			elif isinstance(ele, (type(1), type(1.0))):	
+				list.append(str(ele))		## if an int or float, no quotes
+			else:
+				list.append("'%s'" % str(ele))
+	
+	elif isinstance(arr, type({}) ):		## If the instance is an array
+		for (k,v) in arr.items():
+			item = "'" + str(k) + "'=>"
+			if isinstance(v, ( type([]), type({})) ):
+				item += ( convertArray(v))
+			else:
+				if isinstance(v, (type(1), type(1.0))):	
+					item += (str(v))		## if an int or float, no quotes
+				else:
+					item += ("'%s'" % str(v))
+			list.append(item)
+	else:
+		raise NameError, "Error - neither a array or a dictionary was passed to this function"
+	
+	
+	if len(list) > 0:
+		ret = "array(" + ", ".join(list) + ")"
+	else:
+		ret = "array()"
+		
+	return ret
 
-print a,b
+a = {0:"/ahh/ahhh/hahahaha/ls",1:"ohno/this/is/a/path",2:"dfgdfg"}
+
+print convertArray(a)
