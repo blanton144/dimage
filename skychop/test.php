d2b2686760853b681b02e54d56005af9714b45b2
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-18 03:24:54 +0000

    skychop: swarp php test
    Former-commit-id: 2333616a65441faf402a77c763563ddb12b47d02

--- skychop/test.php
+++ skychop/test.php
@@ -8,7 +8,7 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(E_ALL|E_STRICT);
-	$it = system("/usr/bin/swarp /var/www/html/sdss3/skychop/sdss-tmp/J125148.59+413246.5-clipped-u-192.721_41.1456388691.fits /var/www/html/sdss3/skychop/sdss-tmp/J125152.78+403658.5-clipped-u-192.721_41.0556387165.fits -IMAGEOUT_NAME=/var/www/html/sdss3/skychop/sdss-tmp/test.fits -RESAMPLE_DIR=/var/www/html/sdss3/skychop/sdss-tmp -WEIGHTOUT_NAME=/var/www/html/sdss3/skychop/sdss-tmp/weight.fits 2>&1",$output);
+	$it = system("/usr/bin/swarp /var/www/html/sdss3/skychop/sdss-tmp/J125148.59+413246.5-clipped-u-192.721_41.1456388691.fits /var/www/html/sdss3/skychop/sdss-tmp/J125152.78+403658.5-clipped-u-192.721_41.0556387165.fits -IMAGEOUT_NAME=/var/www/html/sdss3/skychop/sdss-tmp/test.fits -WEIGHTOUT_NAME=/var/www/html/sdss3/skychop/sdss-tmp/weight.fits 2>&1",$output);
 	print_r($output);
 	print_r($it);
 ?>
