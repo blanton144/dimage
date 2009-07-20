06ec9ba5777c8ed370645b6ed8e3c2f6d2600b6e
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-20 18:07:08 +0000

    skychop: chdir instead of abs path
    Former-commit-id: 5fa668959ca2b8013ec1fcc5ce6f9c45699ed2f1

--- skychop/process.php
+++ skychop/process.php
@@ -21,7 +21,8 @@
 	$tar_files = "";
 	
 	if ($proc == 1) {
-		chdir("/var/www/html/sdss3/skychop/sdss-tmp/");
+		$chg = chdir("/var/www/html/sdss3/skychop/sdss-tmp/");
+		print "$chg";
 		$filesToRmv = array(0 => "/var/www/html/sdss3/skychop/sdss-tmp/weight.fits");
 		$pysuccess = exec("/usr/local/epd/bin/python $skychop/find_image.py $RA $dec $sizeX $sizeY $bands $fname 2>&1",$output);
 		for ($i = 0; $i < strlen($bands); $i++) {
