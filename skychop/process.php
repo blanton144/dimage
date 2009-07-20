536c20d19a989c83c1130ed13323a5946a3070b3
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-07-20 14:30:17 +0000

    skychop: investigating swarp error
    Former-commit-id: 0fcef66d93ff9c468ab9091e143e7419590e8a82

--- skychop/process.php
+++ skychop/process.php
@@ -32,10 +32,9 @@
 			$tar_files .= $output[($i * 2) +1];
 		}
 
-		print "tar -cvvf sdss-tmp/$fname.tar$tar_files";
-		//exec("tar -cvvf sdss-tmp/$fname.tar$tar_files");
-		//exec("gzip -c sdss-tmp/$fname.tar > sdss-tmp/$fname.tar.gz");
-		//chmod("sdss-tmp/$fname.tar.gz",0777);
+		exec("tar -cvvf sdss-tmp/$fname.tar $tar_files");
+		exec("gzip -c sdss-tmp/$fname.tar > sdss-tmp/$fname.tar.gz");
+		chmod("sdss-tmp/$fname.tar.gz",0777);
 		
 		/*
 		if ($pysuccess == 1) {
