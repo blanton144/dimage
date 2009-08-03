07f5a1e5b18990ae4570c0b30efda6d06add4f05
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-08-03 17:48:43 +0000

    skychop: cleaned up imports
    Former-commit-id: 4ede0c1d57a20f20ba1c9fbeb7841ecb360afaee

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -45,7 +45,7 @@ allFileNames = None
 # 	 closest mosaic center to that image. Test to make sure that
 #	 the image is unique, and that it lies outside of the closest
 #	 image to the center of the target image.
-for crnr in targetImgCorners: closestCenters.append(ic.findClosestCenter(crnr[0],crnr[1],tableData,np.shape(tableData)[0])))	# Find the closest center to each corner of the target image
+for crnr in targetImgCorners: closestCenters.append(ic.findClosestCenter(crnr[0],crnr[1],tableData,np.shape(tableData)[0]))	# Find the closest center to each corner of the target image
 closestCenters = apw.repDupesWithZero(closestCenters)																			# Replace any duplicates with a 0 to skip
 
 for i in range(len(closestCenters)):
