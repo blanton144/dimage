22299b1077af519f8670c1a18b08339a3831f67f
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-08-12 18:36:28 +0000

    skychop: working again
    Former-commit-id: 46df868d97de60bf532a3cd08f91408737ddbc79

--- skychop/gsky.php
+++ skychop/gsky.php
@@ -313,15 +313,15 @@ function initialize() {
               </tr>
           </table>
       </form>
+      <br />
+      <center><font class='notifyText'>Already have a session ID? <br /><a class='reDLink' href='revisit.php'>Click here to re-download your query results.</a></font></center>
+      <br />
+      <center><font class='notifyText'>Questions? Check the <a class='reDLink' href='FAQ.php'>FAQ.</a></font></center>
+      <br />
+      <center><font class='notifyText'>Report bugs to <a class='reDLink' href='mailto:apw235@nyu.edu'>Adrian Price-Whelan</a></font>
+      <br />
 </td>
 </table>
-<br />
-<center><font class='notifyText'>Already have a session ID? <br /><a class='reDLink' href='revisit.php'>Click here to re-download your query results.</a></font></center>
-<br />
-<center><font class='notifyText'>Questions? Check the <a class='reDLink' href='FAQ.php'>FAQ.</a></font></center>
-<br />
-<center><font class='notifyText'>Report bugs to <a class='reDLink' href='mailto:apw235@nyu.edu'>Adrian Price-Whelan</a></font>
-<br />
 </center>
 </body>
 </html>
