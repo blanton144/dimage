993bda20969d71d700c7181dd9b548a9604e5600
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-08-12 15:21:40 +0000

    skychop: test mode
    Former-commit-id: 9c530cc6d48fa456a0ef3e10c6a02f1fe6a3f606

--- skychop/image_chop.py
+++ skychop/image_chop.py
@@ -74,14 +74,14 @@ def getFileName(theRA, theDec, fitsPath):
 		if fabs(int(degDec)) % 2 == 0: pathDec = fabs(int(degDec))
 		else: pathDec = fabs(int(degDec)) - 1
 		RADecPath = "%(path)s%(hrRa)02dh/p%(dec)02d/" % {"path":fitsPath,"hrRa":hrRa,"dec":pathDec}
+		fileName = "J%(hrRa)02d%(minRa)02d%(secRaINT)02d.%(secRaDECIMAL)02d%(degDec)+03d%(minDec)02d%(secDecINT)02d.%(secDecDECIMAL)01d" % \
+			{"hrRa":hrRa,"minRa":minRa,"secRaINT":secRaINT,"secRaDECIMAL":secRaDECIMAL,"degDec":degDec,"minDec":minDec,"secDecINT":secDecINT,"secDecDECIMAL":secDecDECIMAL}
 	else:
 		if fabs(int(degDec)) % 2 == 0: pathDec = fabs(int(degDec))
 		else: pathDec = fabs(int(degDec)) + 1
 		RADecPath = "%(path)s%(hrRa)02dh/m%(dec)02d/" % {"path":fitsPath,"hrRa":hrRa,"dec":pathDec}
-	
-	fileName = "J%(hrRa)02d%(minRa)02d%(secRaINT)02d.%(secRaDECIMAL)02d%(degDec)+03d%(minDec)02d%(secDecINT)02d.%(secDecDECIMAL)01d" % \
-		{"hrRa":hrRa,"minRa":minRa,"secRaINT":secRaINT,"secRaDECIMAL":secRaDECIMAL,"degDec":fabs(degDec),"minDec":minDec,"secDecINT":secDecINT,"secDecDECIMAL":secDecDECIMAL}
-
+		fileName = "J%(hrRa)02d%(minRa)02d%(secRaINT)02d.%(secRaDECIMAL)02d%(degDec)-03d%(minDec)02d%(secDecINT)02d.%(secDecDECIMAL)01d" % \
+			{"hrRa":hrRa,"minRa":minRa,"secRaINT":secRaINT,"secRaDECIMAL":secRaDECIMAL,"degDec":fabs(degDec),"minDec":fabs(minDec),"secDecINT":fabs(secDecINT),"secDecDECIMAL":fabs(secDecDECIMAL)}
 	return fileName, RADecPath
 
 def getIAUFname(theRA, theDec):
