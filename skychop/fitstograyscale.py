e1289173f1aa8675a18081e37cbac1631d98bcef
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-08-24 15:39:57 +0000

    skychop: new stretching for png
    Former-commit-id: d4be017b619497a4b3d8dcc6170c3b6457e7a24e

--- skychop/fitstograyscale.py
+++ skychop/fitstograyscale.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy.stats import scoreatpercentile
 import sys
 
-def asinhScale(inputArray, scale_min=None, scale_max=None, non_linear=2.0):
+def asinhScale(inputArray, scale=5, non_linear=3):
 	imageData=np.array(inputArray, copy=True)
 	
 	if scale_min == None:
@@ -14,21 +14,18 @@ def asinhScale(inputArray, scale_min=None, scale_max=None, non_linear=2.0):
 	if scale_max == None:
 		scale_max = imageData.max()
 
-	factor = np.arcsinh((scale_max - scale_min)/non_linear)
-	indices0 = np.where(imageData < scale_min)
-	indices1 = np.where((imageData >= scale_min) & (imageData <= scale_max))
-	indices2 = np.where(imageData > scale_max)
+	imageData = np.arcsinh(imageData * factor * non_linear) / non_linear
+	indices0 = np.where(imageData < 0.0)
+	indices2 = np.where(imageData > 1.0)
 	imageData[indices0] = 0.0
 	imageData[indices2] = 1.0
-	imageData[indices1] = np.arcsinh((imageData[indices1] - scale_min) / non_linear) / factor
 	return imageData
 
-beta = 0.8
 file = sys.argv[1]
 pid = sys.argv[2]
 
 dataArray = pyfits.open(file)[0].data
-image = Image.fromarray((255.0*asinhScale(dataArray, scale_max=scoreatpercentile(dataArray.ravel(),100.0), non_linear=beta)).astype('UInt8'), 'L')
+image = Image.fromarray((255.0*asinhScale(dataArray).astype('UInt8'), 'L;I')
 image = image.resize((400,400),Image.BICUBIC)
 image = image.transpose(Image.FLIP_TOP_BOTTOM)
 image.save("%s-asinh.png" % pid)
