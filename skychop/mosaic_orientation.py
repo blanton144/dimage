cd2ff487b37a37e0cd9d14ad14013b4dcf6d73cc
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-08-25 16:42:11 +0000

    skychop: add mosaic orientation
    Former-commit-id: 51e5748ac9e8d6ecf6a6d6cacada3ed4ba1085ca

---
+++ skychop/mosaic_orientation.py
@@ -0,0 +1,60 @@
+#
+#  mosaic_orientation.py
+#  dimage_py
+#
+#  Created by Adrian Price-Whelan on 6/18/09.
+#  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+#
+
+import pyfits as pf
+import numpy as np
+import pylab as pl
+import sys
+import image_chop as ic
+from math import fabs
+import os
+
+tableData = pf.open("sky-patches.fits")[1].data
+RADeg = float(sys.argv[1])
+decDeg = float(sys.argv[2])
+xSize = float(sys.argv[3])
+ySize = float(sys.argv[4])
+
+# RADEC_list format: (RA, Dec, offset)
+RADEC_list = ic.findClosestCenters(RADeg, decDeg, tableData, xSize, ySize)
+
+rectListx,rectListy = [],[]
+targetImgCorners = [(RADeg+xSize/2.0,decDeg+ySize/2.0),(RADeg-xSize/2.0,decDeg+ySize/2.0),(RADeg+xSize/2.0,decDeg-ySize/2.0),(RADeg-xSize/2.0,decDeg-ySize/2.0)]
+oppositeImgCorners = [(RADeg-xSize/2.0,decDeg-ySize/2.0),(RADeg+xSize/2.0,decDeg-ySize/2.0),(RADeg-xSize/2.0,decDeg+ySize/2.0),(RADeg+xSize/2.0,decDeg+ySize/2.0)]
+
+for i in range(len(targetImgCorners)):
+	imName = "test"
+	rectCenter, rectOppCorner = ic.cutSection(targetImgCorners[i], oppositeImgCorners[i], ic.findClosestCenter(targetImgCorners[i][0],targetImgCorners[i][1],tableData),(RADeg,decDeg), (xSize, ySize), tableData)
+	rectListx.append(rectCenter[0])
+	rectListy.append(rectCenter[1])
+
+foundX, foundY = ic.findClosestCenter(RADeg,decDeg,tableData)
+for i in range(np.shape(tableData)[0]):
+	if tableData[i][0] == foundX and tableData[i][1] == foundY:
+		pl.plot([tableData[i][0]-0.5,tableData[i][0]-0.5],[tableData[i][1]-0.5,tableData[i][1]+0.5],'b')
+		pl.plot([tableData[i][0]-0.5,tableData[i][0]+0.5],[tableData[i][1]+0.5,tableData[i][1]+0.5],'b')
+		pl.plot([tableData[i][0]+0.5,tableData[i][0]+0.5],[tableData[i][1]+0.5,tableData[i][1]-0.5],'b')
+		pl.plot([tableData[i][0]+0.5,tableData[i][0]-0.5],[tableData[i][1]-0.5,tableData[i][1]-+0.5],'b')
+	pl.plot([tableData[i][0]-0.5,tableData[i][0]-0.5],[tableData[i][1]-0.5,tableData[i][1]+0.5],'b')
+	pl.plot([tableData[i][0]-0.5,tableData[i][0]+0.5],[tableData[i][1]+0.5,tableData[i][1]+0.5],'b')
+	pl.plot([tableData[i][0]+0.5,tableData[i][0]+0.5],[tableData[i][1]+0.5,tableData[i][1]-0.5],'b')
+	pl.plot([tableData[i][0]+0.5,tableData[i][0]-0.5],[tableData[i][1]-0.5,tableData[i][1]-+0.5],'b')
+	pl.plot([tableData[i][0]], [tableData[i][1]],'b.', ms=5.0)
+
+#print "RA:",foundX, "Dec:", foundY
+#for i in range(np.shape(tableData)[0]):
+#	pl.text(tableData[i][0], tableData[i][1], "RA: %.2f,Dec: %.2f" % (tableData[i][0],tableData[i][1]),fontsize=8)
+pl.plot([RADeg],[decDeg],'r.', ms=10.0)
+
+pl.plot([RADeg-(xSize/2),RADeg-(xSize/2)],[decDeg-(ySize/2),decDeg+(ySize/2)],'r')
+pl.plot([RADeg-(xSize/2),RADeg+(xSize/2)],[decDeg+(ySize/2),decDeg+(ySize/2)],'r')
+pl.plot([RADeg+(xSize/2),RADeg+(xSize/2)],[decDeg+(ySize/2),decDeg-(ySize/2)],'r')
+pl.plot([RADeg+(xSize/2),RADeg-(xSize/2)],[decDeg-(ySize/2),decDeg-+(ySize/2)],'r')
+pl.axis([(RADeg-1),(RADeg+1),decDeg-1,decDeg+1])
+pl.plot(rectListx,rectListy,'g.',ms=10.0)
+pl.show()
