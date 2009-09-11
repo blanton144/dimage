6c7abba9126532bf78f4d8e9d409ceae7876b6c9
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-09-11 20:52:43 +0000

    skychop: server load limit
    Former-commit-id: 4f7514d86be2c10cd75984218200d8cec4fb29f1

--- skychop/process.php
+++ skychop/process.php
@@ -25,6 +25,7 @@
 	// Figure out if the server is busy
 	$filename="pslist.txt"; 
 	$ps = exec('ps aux | awk "/python/ { print \$3 > \"'.$filename.'\" }" 2>&1');
+	/*
 	$cpu_perc = array(); 
 	$file = fopen($filename, "r"); 
 	while(!feof($file)) { 
@@ -104,7 +105,7 @@
 			$site = "process.php?ra=$RA&dec=$dec&xsize=$sizeX&ysize=$sizeY&bands=$bands&fname=$fname&thumb=$thumb&tyn=$thumbYN&proc=1";
 			echo('<meta http-equiv="Refresh" content="1;url='.$site.'">');
 		}
-	}
+	}*/
 ?>
 </body>
 </html>
