31be72e0195a4db33f217e881dac592f2a649491
Adrian Price-Whelan <adrian.prw@gmail.com>
2009-09-14 15:46:40 +0000

    skychop: test python proc. counter
    Former-commit-id: 29fde598efc7b877f200c89e06a3aa35839acd4b

--- skychop/find_image.py
+++ skychop/find_image.py
@@ -64,9 +64,9 @@ for i in range(len(closestCenters)):
 			except IOError:
 				sys.exit("0")
 			ic.clipFits(outDir + fileName + "-" + letter + ".fits", rectCenter[0], rectCenter[1], [rectSize[0],rectSize[1]], \
-				outDir + fileName + "-clipped-" + letter + "-%.2f_%.2f.fits" % (rectCenter[0], rectCenter[1]))
+				outDir + fileName + "-clipped-" + letter + "-%.2f_%.2f-%s.fits" % (rectCenter[0], rectCenter[1],tarName))
 			os.unlink(outDir + fileName + "-" + letter + ".fits")
-			oneImEachBand.append(fileName + "-clipped-" + letter + "-%.2f_%.2f.fits" % (rectCenter[0], rectCenter[1]))
+			oneImEachBand.append(fileName + "-clipped-" + letter + "-%.2f_%.2f-%s.fits" % (rectCenter[0], rectCenter[1],tarName))
 		if allFileNames == None:
 			allFileNames = np.array([oneImEachBand])
 		else:
