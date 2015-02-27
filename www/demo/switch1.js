189aa6784b2b750ae1f9d40a4fd53231dd94dfcf
Michael Blanton <michael.blanton@gmail.com>
2015-02-27 10:13:16 -0500

    remove big files
    Former-commit-id: 2b84b4d9a84a33de012f865147e1f86767b7cb3b

--- www/demo/switch1.js
+++
@@ -1,8 +0,0 @@
-function switch1(div) {
-if (document.getElementById('image1')) {
-var option=['image1','image2'];
-for(var i=0; i<option.length; i++)
-{ obj=document.getElementById(option[i]);
-obj.style.display=(option[i]==div)? "block" : "none"; }
-}
-}
