f6220fad578c499f6328caa4f65b3f8c5f0ffac4
Michael Blanton <michael.blanton@gmail.com>
2015-10-20 21:52:44 -0400

    rearrangement

--- python/dimage/__init__.py
+++ python/dimage/__init__.py
@@ -1,3 +1,9 @@
+from __future__ import division, print_function, unicode_literals
+
+from sdss4tools.install import version
+__version__ = version('$HeadURL$')
+del version
+
 import atlas
 import combine
 import fake
